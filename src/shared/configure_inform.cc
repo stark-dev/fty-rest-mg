4b55bc0e1d86de0ebdd40e10802f77818dd4bab9
Jana Rapava <janarapava@eaton.com>
2017-06-20 16:57:05 +0200

    Problem: DELETE messages are not sent.
    Solution: Always send the message, then trigger UUID generation if necessary.
    
    Signed-off-by: Jana Rapava <janarapava@eaton.com>

--- src/shared/configure_inform.cc
+++ src/shared/configure_inform.cc
@@ -105,15 +105,27 @@ void
             throw std::runtime_error ("persist::select_asset_element_super_parent () failed.");
         }
 
-        // ask fty-asset to republish
-        zmsg_t *republish = zmsg_new ();
-        zmsg_addstr (republish, s_asset_name.c_str ());
-        mlm_client_sendto (client, "asset-agent", "REPUBLISH", NULL, 5000, &republish);
+        zhash_t *ext = s_map2zhash (oneRow.first.ext);
 
-        zhash_destroy (&aux);
+        const char *operation = operation2str (oneRow.second).c_str ();
+        zmsg_t *msg = fty_proto_encode_asset (
+                aux,
+                oneRow.first.name.c_str(),
+                operation,
+                ext);
+        r = mlm_client_send (client, subject.c_str (), &msg);
         if ( r != 0 ) {
             mlm_client_destroy (&client);
             throw std::runtime_error("mlm_client_send () failed.");
+        }
+        zhash_destroy (&ext);
+        zhash_destroy (&aux);
+
+        // ask fty-asset to republish so we would get UUID
+        if (streq (operation, FTY_PROTO_ASSET_OP_CREATE) || streq (operation, FTY_PROTO_ASSET_OP_UPDATE)) {
+            zmsg_t *republish = zmsg_new ();
+            zmsg_addstr (republish, s_asset_name.c_str ());
+            mlm_client_sendto (client, "asset-agent", "REPUBLISH", NULL, 5000, &republish);
         }
 
         //data for uptime
