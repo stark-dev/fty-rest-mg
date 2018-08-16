17b73a70704151b8fbaeb5beadc58e4490867fb4
Jana Rapava <janarapava@eaton.com>
2018-08-16 11:56:19 +0200

    Problem: Build fails because of duplicate definition.
    Solution: Rely on definition in fty-common-db, update includes.
    
    Signed-off-by: Jana Rapava <janarapava@eaton.com>

--- include/shared/configure_inform.h
+++ include/shared/configure_inform.h
@@ -28,6 +28,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 #include "db/dbhelpers.h"
 #include <fty_common.h>
+#include <fty_common_db.h>
 
 void
     send_configure (
