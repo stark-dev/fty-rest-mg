fd07a83591aa9bf5c5ce63125451816f50ecf568
Degott, Francois Regis <FrancoisRegisDegott@eaton.com>
2021-02-01 16:03:51 +0100

    .gitignore + *.xml.exmaple
    Signed-off-by: Degott, Francois Regis <FrancoisRegisDegott@eaton.com>

---
+++ tntnet-install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set +e
+
+make
+
+libname="libfty_rest.so.1.0.0"
+
+libname_src="src/.libs/$libname"
+libname_bios="/usr/lib/bios/$libname"
+
+if [ ! -f "$libname_src" ];
+then
+  echo "$libname_src not found"
+  exit 1
+fi
+
+sudo strip "$libname_src"
+
+if [ ! -f "$libname_bios.original" ];
+then
+  echo "arch $libname_bios.original"
+  sudo mv "$libname_bios" "$libname_bios.original"
+fi
+
+echo "cp $libname_bios"
+sudo cp -f $libname_src $libname_bios
+
+if [ -d "src/web" ];
+then
+  echo "cp xml files in /etc/tntnet/bios.d"
+  ls src/web/*xml
+  #sudo cp src/web/*xml /etc/tntnet/bios.d
+fi
+
+echo "restart tntnet@bios"
+sudo systemctl restart tntnet@bios
+systemctl status tntnet@bios
+
+exit 0
